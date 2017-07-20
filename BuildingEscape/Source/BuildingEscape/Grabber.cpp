9bfc236d65d859161a0a6f62dcd8a0ddeb7f24ed
Daleier <davidjavierbp@gmail.com>
2017-07-20 16:18:59 +0200

    Added ray collision log.

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -44,6 +44,7 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	);
 
 	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
 	//Draw red line
 	DrawDebugLine(
 		GetWorld(),
@@ -55,6 +56,23 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		0.f,
 		10.f
 	);
+
+	//query params setup
+	FCollisionQueryParams TraceParameters(FName(TEXT("")),false,GetOwner());
+
+	//line-trace
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+	);
+	AActor* ActorHit = Hit.GetActor();
+	if (ActorHit) {
+		UE_LOG(LogTemp, Warning, TEXT("Hit %s"), *ActorHit->GetName());
+	}
 
 }
 
